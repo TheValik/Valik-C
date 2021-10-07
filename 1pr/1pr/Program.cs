a7a85b86146226ef1f630073ba9cc036050dea47
TheValik <valik030212@gmail.com>
2021-10-07 18:30:13 +0300

    Finish

--- 1pr/1pr/Program.cs
+++ 1pr/1pr/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace _1pr
 {
@@ -6,7 +7,34 @@ namespace _1pr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random r0 = new Random(0);
+            Random r1 = new Random(0);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(r0.Next(0, 100));
+            }
+            Console.WriteLine("---------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(r1.Next(0, 100));
+            }
+            Console.WriteLine("---------------------");
+            Random r2 = new Random(1);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(r2.Next(0, 100));
+            }
+            Console.WriteLine("---------------------");
+            var rnd0 = new RNGCryptoServiceProvider();
+            var rndGen = new byte[10];
+
+            for(int i=0; i<5; i++)
+            {
+                rnd0.GetBytes(rndGen);
+                string text = Convert.ToBase64String(rndGen);
+                Console.WriteLine(text);
+            }
+
         }
     }
 }
