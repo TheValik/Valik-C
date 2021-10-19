3271aceb946397c117695aa46ee439f3930f1c34
TheValik <90153132+TheValik@users.noreply.github.com>
2021-10-19 15:10:38 +0300

    sprob1

---
+++ 2 ПЗ/Пр2/Пр2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Пр2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte key = 2;
+
+            //зчитування елементів файлу в масив перетворюючи елементи в байти
+            byte[] decData = File.ReadAllBytes("file.TXT").ToArray();
+            //створення пустих масивів типу byte
+            byte[] arr1 = new byte[3];
+            byte[] arr2 = new byte[3];
+            Console.WriteLine("-------------");
+            //виведення елементів тексту
+            Console.WriteLine("Не зашифрований текст");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine((char)decData[i]);
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("Зашифрований текст");
+            //шифрування
+            for (int i = 0; i < 3; i++)
+            {
+                arr1[i] = (byte)(decData[i] ^ key);
+            }
+            //виведення зашифрованого
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine((char)arr1[i]);
+            }
+            Console.WriteLine("-------------");
+
+            File.WriteAllBytes("text.dat", arr1);
+            //----------------------------------
+
+            Console.WriteLine("-------------");
+            Console.WriteLine("Розшифрований файл");
+            byte[] shyfr = File.ReadAllBytes("text.dat").ToArray();
+            for (int i = 0; i < 3; i++)
+            {
+                arr2[i] = (byte)(arr1[i] ^ key);
+                Console.WriteLine((char)arr2[i]);
+
+            }
+        }
+    }
+}
---
+++ 2 ПЗ/взлом/взлом/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Пр2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            StreamReader sorse = new StreamReader("C:\\encfile5.dat");
+            string text = sorse.ReadToEnd();
+            string slov = "Mit21";
+            int lich = 1;
+            int stop = 0;
+
+            var password = new StringBuilder(5);
+            for(char a = '!'; a < '~'; a++)
+            {
+                for(char b = '!'; b < '~'; b++)
+                {
+                    for(char c='!'; c < '~'; c++)
+                    {
+                        for(char d = '!'; d < '~'; d++)
+                        {
+                            for(char e = '!'; e < '~'; e++)
+                            {
+                                password.Clear();
+
+
+                                password.Append(a);
+                                password.Append(b);
+                                password.Append(c);
+                                password.Append(d);
+                                password.Append(e);
+
+                                for(int i =0;i< text.Length; i++)
+                                {
+
+                                    if((char)(byte)(text[i] ^ password[0])==slov[0])
+                                    {
+                                        for(int per = 1; per < slov.Length; per++)
+                                        {
+                                            if((char)(byte)(text[i+per] ^ password[per]) == slov[per])
+                                            {
+                                                lich++;
+                                            }
+                                            //умова виходу
+                                            if (lich == 5)
+                                            {
+                                                Console.WriteLine("gotovo");
+                                                stop = 1;
+                                            }
+                                        }
+                                        //Скидання лічильника
+                                        lich = 1;
+                                    }
+                                    if (stop == 1)
+                                    {
+                                        break;
+                                    }
+                                }
+                                if (stop == 1)
+                                {
+                                    break;
+                                }
+                            }
+                            
+                            if (stop == 1)
+                            {
+                                break;
+                            }
+                        }
+                        if (stop == 1)
+                        {
+                            break;
+                        }
+                    }
+                    if (stop == 1)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("a= " + a);
+                if (stop == 1)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(password);
+
+            int k = 0;
+           for(int i = 0; i < text.Length; i++)
+            {
+                Console.Write((char)(byte)(text[i] ^ password[k]));
+                k++;
+                if (k > 4)
+                {
+                    k = 0;
+                }
+            }
+ 
+
+
+
+
+
+        }
+    }
+}
