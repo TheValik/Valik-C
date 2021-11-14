d0398d8f600b8abdb25581d499e2bf562a51baa4
TheValik <90153132+TheValik@users.noreply.github.com>
2021-11-14 21:44:02 +0200

    PZ7

---
+++ 6 ПЗ/ПЗ№7/zavd_2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ПЗ_7
+{
+      public class RSAWithRSAParameterKey
+        {
+            
+            // Збереження закритого ключа
+            private readonly static string CspContainerName = "RsaContainer";
+            public static void AssignNewKey3()
+            {
+                CspParameters cspParameters = new CspParameters(1)
+                {
+                    KeyContainerName = CspContainerName,
+
+                    ProviderName = "Microsoft Strong Cryptographic Provider"
+                };
+                var rsa = new RSACryptoServiceProvider(cspParameters)
+                {
+                    PersistKeyInCsp = true
+                };
+                File.WriteAllText("F:/Навчання/С#/6 ПЗ/test5.xml", rsa.ToXmlString(false));
+            }
+
+            // Розшифрування
+            public static byte[] DecryptData(byte[] dataToDecrypt)
+            {
+                byte[] plainBytes;
+                var cspParams = new CspParameters
+                {
+                    KeyContainerName = CspContainerName,
+                    
+                };
+                using (var rsa = new RSACryptoServiceProvider(cspParams))
+                {
+                    rsa.PersistKeyInCsp = true;
+                    plainBytes = rsa.Decrypt(dataToDecrypt, false);
+                }
+                return plainBytes;
+            }
+
+            // Шифрування
+            public static byte[] EncryptData2(string publicKeyPath,byte[] dataToEncrypt)
+            {
+                byte[] cipherbytes;
+                using (var rsa = new RSACryptoServiceProvider(2048))
+                {
+                    rsa.PersistKeyInCsp = false;
+                    rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+                    cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+                }
+                return cipherbytes;
+            }
+        }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string a = "Hello World!";
+            RSAWithRSAParameterKey.AssignNewKey3();
+            var Encr = RSAWithRSAParameterKey.EncryptData2("F:/Навчання/С#/6 ПЗ/test5.xml", Encoding.Unicode.GetBytes(a));
+            var Decr = RSAWithRSAParameterKey.DecryptData(Encr);
+
+
+            Console.WriteLine("Просте повiдомлення:  " + a);
+            Console.WriteLine("");
+            Console.WriteLine("Зашифроване повiдомлення:  " + Encoding.Unicode.GetString(Encr));
+            Console.WriteLine("");
+            Console.WriteLine("Розшифроване повiдомлення:  " + Encoding.Unicode.GetString(Decr));
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ 6 ПЗ/ПЗ№7/ПЗ№7/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ПЗ_7
+{
+
+    class Program
+    {
+
+        public class RSAWithRSAParameterKey
+        {
+            private static RSAParameters _publicKey;
+            private static RSAParameters _privateKey;
+
+            // In-Memory Keys
+            public static void AssignNewKey()
+            {
+                using (var rsa = new RSACryptoServiceProvider(4096))
+                {
+                    rsa.PersistKeyInCsp = false;
+                    _publicKey = rsa.ExportParameters(false);
+                    _privateKey = rsa.ExportParameters(true);
+                }
+            }
+
+            // Шифрування
+            public static byte[] EncryptData(byte[] dataToEncrypt)
+            {
+                byte[] cypherBytes;
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.PersistKeyInCsp = true;
+                    rsa.ImportParameters(_publicKey);
+                    cypherBytes = rsa.Encrypt(dataToEncrypt, true);
+                }
+                return cypherBytes;
+            }
+
+
+            // Розшифрування
+            public static byte[] DecryptData(byte[] dataToDecrypt)
+            {
+                byte[] plainBytes;
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.PersistKeyInCsp = true;
+                    rsa.ImportParameters(_privateKey);
+                    plainBytes = rsa.Decrypt(dataToDecrypt, true);
+                }
+                return plainBytes;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string a = "Hello World!";
+            RSAWithRSAParameterKey.AssignNewKey();
+            var EncrB = RSAWithRSAParameterKey.EncryptData(Encoding.Unicode.GetBytes(a));
+            var DecrB = RSAWithRSAParameterKey.DecryptData(EncrB);
+
+            
+
+            Console.WriteLine("Просте повiдомлення:  " + a);
+            Console.WriteLine("");
+            Console.WriteLine("Зашифроване повiдомлення:  " + Encoding.Unicode.GetString(EncrB));
+            Console.WriteLine("");
+            Console.WriteLine("Розшифроване повiдомлення:  " + Encoding.Unicode.GetString(DecrB));
+            Console.WriteLine("");
+
+        }
+    }
+}
